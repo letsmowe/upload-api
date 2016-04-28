73d24c81d504532564c19f12314428ea437072df
Cristiano Fromagio <fromagio_cristiano@live.com>
2016-04-28 14:01:10 -0300

    PHP handling uploaded files through post

--- api/index.php
+++ api/index.php
@@ -22,27 +22,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 class Response {
 
-	public $sent;
-
-	protected $mailer;
+	public $files;
+	public $req;
 
 	/**
 	 * Response constructor.
-	 * @param $post {array}
-	 * @param $mailer {PHPMailer}
-	 * @internal param bool $sent
+	 * @param $files {array}
+	 * @param $req {array}
 	 */
-	public function __construct($post)
+	public function __construct($files, $req)
 	{
-		$this->post = $post;
-	}
-
-	/**
-	 * @param mixed $sent
-	 */
-	public function setSent($sent)
-	{
-		$this->sent = $sent;
+		$this->files = $files;
+		$this->req = $req;
 	}
 
 	/**
@@ -52,12 +43,65 @@ class Response {
 	{
 		return json_encode($this);
 	}
-
 }
 
-$response = new Response($_POST);
+class File {
+	public $origin;
+	public $temp_path;
+	public $name;
 
-if ($_GET['callback'])
-	echo $_GET['callback'] . '(' . json_encode($_POST) . ')';
-else
-	echo json_encode($_POST);
+	/**
+	 * File constructor.
+	 * @param $origin
+	 * @param $temp_path
+	 */
+	public function __construct($origin, $temp_path)
+	{
+		$this->origin = $origin;
+		$this->temp_path = $temp_path;
+		$this->handleFile($this->origin,$this->temp_path);
+	}
+
+	/**
+	 * @param $origin (nome de origem, no envio)
+	 * @param $temp_path (caminho, do diretório temporário)
+	 */
+	public function handleFile ($origin, $temp_path)
+	{
+		$updir = '/var/www/static/';
+		$info = pathinfo($origin);
+		$upext = $info['extension'];
+		$upfile = random_int(0, 999999);
+
+		if (move_uploaded_file($temp_path, $updir . $upfile . "." . $upext)) {
+			$this->name = $upfile . "." . $upext;
+			echo $this->toJSON();
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function toJSON()
+	{
+		return json_encode($this);
+	}
+}
+
+try {
+	if ($_GET['callback'])
+		echo $_GET['callback'] . '(' . json_encode($_GET) . ')'; //jsonp apenas requisições get
+	else if (count($_POST)) { //$_POST é um array
+		//ignorando o campo texto por enquanto:
+		$temppath = $_FILES["file"]["tmp_name"];
+		$originname = $_FILES["file"]["name"];
+		$file = new File($originname, $temppath);
+	} else {
+		//get? > listar arquivos
+	}
+} catch (Exception $e) {
+	echo "Erro: " . $e->getMessage();
+}
+
+//	if ($_POST["file"] != "undefined")
+//		echo json_encode(array($_FILES, $_POST));
